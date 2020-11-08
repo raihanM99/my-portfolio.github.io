b98d144986d27f167f6906562edb415cdcb677fd
raihanM99 <mraihanm99@gmail.com>
2020-11-08 21:00:38 +0700

    Menambahkan smoot scrooll efek parallax

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,105 @@ $('.page-scroll').on('click', function(e){
   const href = $(this).attr('href');
   // tangkap element
   const elementHref = $(href);
-  $('html,body').animate({
-    scrollTop: elementHref.offset().top - 50
-  }, 800);
+  // console.log(elementHref[0].id);
+  if (elementHref[0].id === 'portfolio') {
+    $('html,body').animate({
+      scrollTop: elementHref.offset().top - 62.3
+    }, 800, 'swing');
+  } else {
+    $('html,body').animate({
+      scrollTop: elementHref.offset().top - 65
+    }, 800, 'swing');
+  }
 
   e.preventDefault();
+});
+
+$(window).scroll(function() {
+  const windowScroll = $(this).scrollTop();
+  console.log(windowScroll);
+
+  // nav
+
+  
+
+  $('nav').css({
+    'background-color' : 'rgb(33, 37, 41)',
+  });
+
+  if (windowScroll >= 373) {
+    $('nav').css({
+      'background-color' : 'rgba(33, 37, 41, .8)',
+      'transition' : '.5s'
+    });
+  }
+
+  // Jumbotron
+
+  $('.jumbotron img').css({
+    'transform' : `translate(0px,${windowScroll/4}%`,
+    'opacity' : '1',
+    'transition' : '.6s'
+  });
+
+  $('.jumbotron h1').css({
+    'transform' : `translate(0px,${windowScroll}%`,
+    'opacity' : '1',
+    'transition' : '.6s'
+  });
+
+  $('.jumbotron hr').css({
+    'opacity' : '1',
+    'transform' : `translate(0px,${windowScroll/6}%`,
+    'transition' : '.6s'
+  });
+
+  $('.jumbotron p').css({
+    'transform' : `translate(0px,${windowScroll}%`,
+    'opacity' : '1',
+    'transition' : '.6s'
+  });
+
+  if (windowScroll > 90) {
+    $('.jumbotron hr').css({
+      'opacity' : `0`,
+      'transform' : `translate(50px,${windowScroll/6}%`,
+      'transition' : '.5s'
+    });
+  }
+  if (windowScroll > 340) {
+    $('.jumbotron p').css({
+      'transform' : `translate(0px,${windowScroll}%`,
+      'opacity' : '0',
+      'transition' : '.5s'
+    });
+
+    $('.jumbotron h1').css({
+      'transform' : `translate(0px,${windowScroll}%`,
+      'opacity' : '0',
+      'transition' : '.5s'
+    });
+
+    $('.jumbotron img').css({
+      'transform' : `translate(0px,${windowScroll/4}%`,
+      'opacity' : '0',
+      'transition' : '1s'
+    });
+  }
+
+  // Portfolio
+
+  $('.portfolio .card').css({
+    'opacity' : '0',
+    'transition' : '1.5s',
+    'transform' : 'translate(0,-40px)'
+  });
+
+  if (windowScroll >= 780) {
+    $('.portfolio .card').css({
+      'opacity' : '1',
+      'transition' : '1s',
+      'transform' : 'translate(0,0)'
+    })
+  }
 })
